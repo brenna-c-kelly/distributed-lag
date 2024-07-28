b16396ea69f29798b3a431e1ed2a4ba4cc586930
Brenna Kelly <brennackelly@icloud.com>
2024-07-28 11:51:06 -0600

    formalizing case_ts

--- 00_case ts.R
+++ 00_case ts.R
@@ -1,6 +1,7 @@
 
 
 library(gnm)
+library(lme4)
 library(dlnm)
 library(tidyr)
 library(dplyr)
@@ -22,23 +23,26 @@ case$conception_date <- case$birth_date - (case$gestation)*7
 case$conception_week <- case$week - case$gestation
 case <- rename(case, birth_week = week)
 
+head(case)
+
 case <- case |>
   filter(conception_date > "2013-01-01")
 
 # constructing the composite outcome > outcomes related to placental insufficiency
 case$pl_insuf <- case_when(case$sga10_by_sex > case$birthweightgrams ~ 1, # sga
-                           case$placenta_abr == 1 ~ 1)
+                           case$hypertension %in% c("H", "E", "P") ~ 1)
+                           # case$placenta_abr == 1 ~ 1)
 case$pl_insuf <- ifelse(is.na(case$pl_insuf), 0, 1)
 
 case <- case[, c("grid_id",  "momid", "birth_week",
-                 "gestation", "placenta_abr", "sga",
+                 "gestation", "pl_insuf",
                  "conception_week")]
 
 head(case)
 
 grid_weeks <- case[, c("grid_id", "momid",
                        "conception_week",
-                       "birth_week")]
+                       "birth_week", "pl_insuf")]
 
 expand_preg_fx <- function(x) {
   
@@ -72,38 +76,138 @@ preg_exp$pregnancy_week <- gsub("week_", "", preg_exp$date_type)
 # ggplot(preg_exp, aes(x = exposure_week, y = mean_no2)) +
 #   geom_point(alpha = 0.01)
 
-head(preg_exp)
-
 early_preg_exp <- preg_exp |>
   mutate(pregnancy_week = as.numeric(pregnancy_week)) |>
-  filter(pregnancy_week <= 20)
+  filter(pregnancy_week <= 25)
 
 
-## build the cross-basis of predictors and lags
+## build the cross-basis of pm
 cb1.pm <- crossbasis(early_preg_exp$max_pm, 
-                     lag = 20, 
-                     argvar = list(fun="ns", knots = 4), 
-                     arglag = list(fun="ns", knots = 4))
+                     lag = 25, 
+                     argvar = list(fun="ns", knots = 3), 
+                     arglag = list(fun="lin"))
 summary(cb1.pm)
 
-# test <- merge(early_preg_exp, case, by = "momid", all.x = TRUE)
-
-m <- glm(case ~ 1 + cb1.pm, # (1 | week)
+## pm
+m <- glm(pl_insuf ~ 1 + cb1.pm, # (1 | week)
          data = early_preg_exp, family = "binomial")
 
-pred1.pm <- crosspred(cb1.pm, m, at=2.5:141, bylag=1, cumul=TRUE)
+pred1.pm <- crosspred(cb1.pm, m, at = 0:200,#0.27:199, 
+                      bylag = 1, cumul = TRUE, cen = mean(early_preg_exp$mean_pm))
 
 plot(pred1.pm, col = "blue")
 
+pred1.pm <- crosspred(cb1.pm, m, at = 25, bylag = 1, cumul = TRUE, 
+                      cen = mean(early_preg_exp$mean_pm))
+
+plot(pred1.pm, var = 25, xlim = c(0, 25), xlab = "Lag from 25 (weeks)", ylab = "OR for maxpm = 50",
+     ylim = c(0.9,1.1), lab = c(8,5,5), col = 1, lwd = 1.5, ci = "area")
 
 
-cbpoll <- crossbasis(data$pollen, lag=3, argvar=list(knots=c(40,100)),
-                     arglag=list(knots=1), group=data$id)
-cbpm <- crossbasis(data$pm, lag=3, arglag=list("integer"), group=data$id)
-cbtmean <- crossbasis(data$tmean, lag=3, argvar=list(knots=1:2*10),
-                      arglag=list(knots=1), group=data$id)
+## no2
+cb1.no2 <- crossbasis(early_preg_exp$max_no2, 
+                      lag = 25, 
+                      argvar = list(fun = "ns", knots = 4), 
+                      arglag = list(fun = "ns", knots = 4))
+summary(cb1.no2)
+
+m <- glm(pl_insuf ~ 1 + cb1.no2, # (1 | week)
+         data = early_preg_exp, family = "binomial")
+
+pred1.no2 <- crosspred(cb1.no2, m, at=0.1:192, bylag=1, cumul=TRUE)
+
+plot(pred1.no2, col = "orange")
+
+pred1.no2 <- crosspred(cb1.no2, m, at = 50, bylag = 1, cumul = TRUE)
+
+plot(pred1.no2, var = 50, xlim = c(0, 25), xlab = "Lag (weeks)", ylab = "OR for max_no2 = 70",
+     ylim = c(0.95, 1.05), lab = c(8,5,5), col = 1, lwd = 1.5, ci = "area")
+
+## temp
+cb1.temp <- crossbasis(early_preg_exp$max_temp, 
+                      lag = 25, 
+                      argvar = list(fun = "lin"), 
+                      arglag = list(fun="ns", knots = 3))
+summary(cb1.temp)
+
+m <- glm(pl_insuf ~ 1 + cb1.temp, # (1 | week)
+         data = early_preg_exp, family = "binomial")
+exp(m$coefficients[[1]])
+pred1.temp <- crosspred(cb1.temp, m, at=251:301, bylag=1, cumul=TRUE)
+
+plot(pred1.temp, col = "red")
+
+pred1.temp <- crosspred(pred1.temp, m, at = 300, bylag = 1, cumul = TRUE)
+
+plot(pred1.temp, var = 300, xlim = c(0, 20), xlab = "Lag (weeks)", ylab = "HR for 100 WLM/year",
+     ylim = c(0.9,1.1), lab = c(8,5,5), col = 1, lwd = 1.5, ci = "area")
+
+
+## o3
+cb1.o3 <- crossbasis(early_preg_exp$mean_o3, 
+                       lag = 25, 
+                       argvar = list(fun="ns", knots = 4),
+                       arglag = list(fun="ns", knots = 3))
+summary(cb1.o3)
+early_preg_exp$momid <- as.factor(early_preg_exp$momid)
+early_preg_exp$exposure_week <- as.factor(early_preg_exp$exposure_week)
+
+# m <- glmer(pl_insuf ~ 1 + (1 | momid) + 1(),# + cb1.o3, 
+#          data = early_preg_exp, family = "binomial", link = "log")
+
+m_simple <- glm(pl_insuf ~ 1 + cb1.o3, 
+         data = early_preg_exp, family = "binomial")
+
+pred1.o3 <- crosspred(cb1.o3, m, at = 7.5:91, bylag = 1, cumul= TRUE)
+
+plot(pred1.o3, col = "darkolivegreen1")
+
+pred1.o3 <- crosspred(cb1.o3, m, at = 60, bylag = 1, cumul = TRUE)
+
+plot(pred1.o3, var = 60, xlim = c(0, 20), xlab = "Lag (weeks)", ylab = "HR for 100 WLM/year",
+     ylim = c(0.9,1.1), lab = c(8,5,5), col = 1, lwd = 1.5, ci = "area")
 
 
 
 
+library(INLA)
+cb1.temp <- crossbasis(early_preg_exp$mean_temp, 
+                     lag = 25, 
+                     argvar = list(fun="lin"),
+                     arglag = list(fun="lin"))
+pred1.temp <- crosspred(cb1.temp, m_inla, at=247:298, bylag=1, cumul=TRUE)
+summary(cb1.temp)
 
+str(cb1.temp)
+
+
+system.time(
+  m_inla <- inla(pl_insuf ~ 1 + f(momid, model = "iid") + 
+                   f(exposure_week, model = "iid"),
+                 data = early_preg_exp,
+                 verbose = FALSE,
+                 family = "zeroinflatedbinomial1") # runs in 23m
+)
+
+summary(m_inla)
+
+plot(m_inla, plot.fixed.effects = FALSE,
+     plot.random.effects = FALSE,
+     plot.hyperparameters = TRUE,
+     plot.predictor = FALSE, cex = 1.25)
+
+1 - exp(m_inla$summary.fixed$mean) # predicted: 84%, slight improvement with ZIB_1
+prop.table(table(early_preg_exp$pl_insuf)) # observed: 89%
+# underfitting zeros
+
+
+# cbpoll <- crossbasis(data$pollen, lag=3, argvar=list(knots=c(40,100)),
+#                      arglag=list(knots=1), group=data$id)
+# cbpm <- crossbasis(data$pm, lag=3, arglag=list("integer"), group=data$id)
+# cbtmean <- crossbasis(data$tmean, lag=3, argvar=list(knots=1:2*10),
+#                       arglag=list(knots=1), group=data$id)
+# 
+# 
+# 
+# 
+# 
--- 01_placental insufficiency.R
+++ 01_placental insufficiency.R
@@ -1,7 +1,13 @@
 
+library(sf)
 library(MASS)
+library(lme4)
+library(INLA)
 library(dplyr)
 library(moments)
+library(splines)
+library(data.table)
+library(tidycensus)
 library(performance)
 
 case <- read.csv("/Users/brenna/Downloads/clean_case_data.csv")
@@ -30,6 +36,75 @@ grid_dat <- data.frame(grid_id = nu$Group.1,
 grid_dat <- merge(grid_dat, denom, by = c("grid_id", "week"), all.x = TRUE)
 head(grid_dat)
 
+
+
+slc_shp <- get_acs(geography = "county",
+                   state = "UT",
+                   variables = c("B03002_001"), # bach or higher 
+                   geometry = TRUE,
+                   year = 2021) |>
+  filter(GEOID == "49035")
+
+slc_shp <- st_transform(slc_shp, st_crs(grid))
+
+grid <- st_read("/Users/brenna/Downloads/shapefiles") |>
+  dplyr::select(grid_id)
+
+# test <- st_join(dat_aq_shp, slc_shp, join = st_intersects) |>
+#    filter(!is.na(GEOID))
+
+case_sm <- case[, c("grid_id", "momid", "gestation", "week", "pl_insuf")]
+
+case_sm$start_week <- case_sm$week - case_sm$gestation
+
+case_sm <- case_sm |> 
+  filter(start_week > 0) |>
+  filter(momid %in% c(646037463, 616693064))
+
+case_aq <- merge(aq, case_sm, by = "grid_id", all.x = FALSE)
+case_aq <- case_aq |>
+  filter(week.x >= start_week &
+           week.y >= week.x) |>
+  rename(birth_week = week.y,
+         week = week.x)
+write.csv(case_aq, "case-poll.csv", row.names = FALSE)
+
+aq <- fread("/Users/brenna/Downloads/clean_dataset.csv")
+
+
+aq <- aq |>
+  filter(grid_id %in% grid_dat$grid_id)
+
+dat_shp <- merge(grid, grid_dat, by = c("grid_id"), all.y = TRUE)
+
+dat_aq_shp <- merge(grid_dat,#dat_shp, )
+                    aq, by = c("grid_id", "week"))
+
+small_poll <- dat_aq_shp |>
+  filter(grid_id %in% c(1292159, 1293358, 1293359, 1293360))
+aq <- aq |>
+  filter(grid_id %in% c(1292159, 1293358, 1293359, 1293360))
+
+write.csv(small_poll, "grid-week-cases.csv", row.names = FALSE)
+write.csv(aq, "grid-week-poll.csv", row.names = FALSE)
+
+write.csv(dat_aq_shp, "all-grid-week-cases.csv", row.names = FALSE)
+write.csv(aq, "all-grid-week-poll.csv", row.names = FALSE)
+
+sort(unique(small_poll$week))
+sort(unique(grid_dat$week))
+
+
+dat_aq_shp <- st_drop_geometry(dat_aq_shp)
+
+head(aq)
+
+slc_shp <- slc_shp |>
+  filter(GEOID == )
+
+# dat_aq_shp <- 
+
+
 m <- glm(case ~ 1 + offset(denom),
          family = "poisson",
          data = grid_dat)
@@ -43,16 +118,61 @@ skewness(grid_dat$denom_log)
 # mm that didn't help much
 
 m <- glm.nb(case ~ 1 + offset(denom),
-         data = grid_dat)
+         data = dat_aq_shp)
 check_overdispersion(m) # it's close, but no
 
 summary(m)
 exp(m$coefficients)
 
 
+## build the cross-basis of predictors and lags
+cb1.pm <- crossbasis(dat_aq_shp$max_pm, 
+                     lag = 40, 
+                     argvar = list(fun="ns", knots = 4), 
+                     arglag = list(fun="ns", knots = 4))
+summary(cb1.pm)
+cb1.no2 <- crossbasis(dat_aq_shp$max_no2, 
+                     lag = 40, 
+                     argvar = list(fun = "lin"), 
+                     arglag = list(fun="ns", knots = 3))
+summary(cb1.no2)
 
 
-model1 <- glm(death ~ cb1.pm + cb1.temp + ns(time, 7*14) + dow,
-              family = quasipoisson(), cases_grid)
+case_grid <- aggregate(dat_aq_shp$case, by = list(dat_aq_shp$grid_id), FUN = sum)
+
+dat_aq_shp_small <- dat_aq_shp[which(dat_aq_shp$grid_id == 1829016), ]
+
+# model
+m <- glm.nb(case ~ 1 + cb1.pm + offset(denom), # (1 | week)
+            data = dat_aq_shp)
+# glmer.nb
+# crosspred(cb1.pm, m, at=0.7:197, bylag=1, cumul=TRUE)
+
+summary(m)
+
+pred1.pm <- crosspred(cb1.pm, m, at=2.5:141, bylag=1, cumul=TRUE)
+pred1.no2 <- crosspred(cb1.no2, m, at=0.5:190, bylag=1, cumul=TRUE)
+
+plot(pred1.pm, col = "blue")
+plot(pred1.no2)
+
+summary(m)
+summary(pred1.pm$predvar)
+
+pred1.pm <- crosspred(cb1.pm, m, at=70, bylag=1, cumul=TRUE)
+pred1.no2 <- crosspred(cb1.no2, m, at=150, bylag=1, cumul=TRUE)
+
+plot(pred1.pm,var=70,xlim=c(0,40),xlab="Lag (years)",ylab="HR for 100 WLM/year",
+     ylim=c(0.9,1.3),lab=c(8,5,5),col=1,lwd=1.5,ci="n")
+plot(pred1.no2,var=150,xlim=c(0,40),xlab="Lag (years)",ylab="HR for 100 WLM/year",
+     ylim=c(0.9,1.3),lab=c(8,5,5),col=1,lwd=1.5,ci="n")
+
+
+## with inla
+system.time(
+  n <- inla(case ~ 1 + cb1.pm + f(week, model = "rw1") + 
+              f(grid_id, model = "iid") + offset(denom),
+            data = dat_aq_shp, family = "nbinomial")
+)
 
 
